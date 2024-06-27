6853b2c1e7b07d8cce17382d6488e81e2aeee631
Lomalseba <Lomalseba>
2024-06-27 21:28:59 +0300

    fef

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY = os.urandom(24)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///local_database.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+USER_ROLE = 1
+MODERATOR_ROLE = 2
+ADMINISTRATOR_ROLE = 3
+
